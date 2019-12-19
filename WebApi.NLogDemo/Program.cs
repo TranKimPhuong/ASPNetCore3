6032ed2bfa7e19fa09797311973210d2fcf21434
Phuong Tran Thi Kim <kimphuong.tran@EITSVN.COM>
2019-12-19 13:15:09 +0700

    Test NLog. Van chua log on Azure dc

---
+++ WebApi.NLogDemo/Program.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Web;
+
+namespace WebApi.NLogDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Info("hello world!!!!");
+                
+                CreateHostBuilder(args).Build().Run();
+            }
+            finally
+            {
+                LogManager.Shutdown();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureLogging(logging =>
+                        logging.ClearProviders()
+                )
+                .UseNLog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ WebApi.NLogDemo/README.txt
@@ -0,0 +1,11 @@
+﻿Follow to https://medium.com/@jocapc/configuring-nlog-in-asp-net-core-web-app-59d1ab3697de
+	and https://www.codeproject.com/Questions/4386550/Why-my-logs-file-not-creating-on-azure-blob-storag
+
+Install packages
+	NLog.Web.AspNetCore
+	NLog.Extensions.AzureStorage
+
+=> Ra file + console => ok
+=>	Emulator : ko support
+	Container bt : ko có tableName vì dang xài NLog.Extensions.AzureStorage 
+				   not NLog.Extensions.AzureTableStorage
